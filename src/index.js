cb548b2d53ace9f9886081a5e013dd24f20137e2
hashidk <wandres_mv1@hotmail.com>
2022-12-02 11:13:53 -0500

    [hashidk]: Añadiendo el servidor nodeJS

---
+++ src/index.js
@@ -0,0 +1,10 @@
+
+const express = require("express")
+const app = express()
+require('dotenv').config()
+
+app.use(express.static('public'))
+
+app.listen(process.env.PORT, function () {
+    console.log("Servidor abierto en: "+process.env.URL+":"+process.env.PORT);
+})
